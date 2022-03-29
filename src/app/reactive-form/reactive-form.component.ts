fa436ec8dc3ca59af1b097df37514d0f38a47674
Jonathan Ndambuki <joendambuki16@gmail.com>
2022-03-29 16:41:55 +0300

    Solved Bugs and finished on Reactive forms

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
@@ -8,17 +9,61 @@ import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/fo
 export class ReactiveFormComponent implements OnInit {
   genders=['Male', 'Female']
   signUpform!:FormGroup
+  forbiddenUsernames=['joe', 'hacker', 'incognito']
   constructor() { }
 
   ngOnInit(): void {
     this.signUpform= new FormGroup({
       'userData':new FormGroup({
-        'username':new FormControl(null,Validators.required),
-        'email':new FormControl(null,[Validators.email, Validators.required]),
+        'username':new FormControl(null,[Validators.required
+          ,this.forbidUsernames.bind(this)]),
+        'email':new FormControl(null,[Validators.email,
+           Validators.required]),
       }),
       'gender':new FormControl(null, Validators.required),
-      'hobbies': new FormArray([])
+      'hobbies': new FormArray([]),
+      'password':new FormControl(null,
+        [Validators.required,  
+          this.checkcapital.bind(this), this.checknumeric.bind(this) ,
+          this.checkSpecial.bind(this),
+         ]
+        )
     })
+
+    // this.signUpform.valueChanges.subscribe(
+    //   value=>{
+    //     console.log(value);
+        
+    //   }
+// )
+    this.signUpform.statusChanges.subscribe(
+      status=>{
+        console.log(status);
+        
+      }
+    )
+
+    // this.signUpform.setValue({
+    //   'userData':{
+    //     'username':'Joe',
+    //     'email':'Joe@gmail.com'
+    //   },
+    //   'gender':'Male',
+    //   'hobbies':[],
+    //   'password':'12345e'
+    // })
+
+    
+    this.signUpform.patchValue({
+      'userData':{
+        'username':'Joe',
+        'email':'Joe@gmail.com'
+       },
+      // 'gender':'Male',
+      // 'hobbies':[],
+      // 'password':'12345e'
+    })
+  
   }
 
   OnSubmit(){
@@ -32,8 +77,54 @@ export class ReactiveFormComponent implements OnInit {
     )
   }
 
+
   getControls() {
     return (<FormArray>this.signUpform.get('hobbies')).controls;
   }
+
+  // :{[s:string]:boolean} | null
+
+    forbidUsernames(contol:FormControl){
+        if(this.forbiddenUsernames.indexOf(contol.value)!== -1){
+          return{'nameForbidden': true}
+        }
+        return null
+        
+    }
+
+    checkcapital(contol:FormControl){
+       const value=contol.value
+        const hasUpperCase = /[A-Z]+/.test(value)
+       return ! hasUpperCase? {'hasCapital': true}:null
+
+      
+  }
+  checkSpecial(contol:FormControl){
+    const value=contol.value
+     const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value)
+    return ! hasSpecial? {'hasSpecial': true}:null
+}
+  
+  checknumeric(contol:FormControl){
+    const value=contol.value
+   const hasNumeric =  /[0-9]+/.test(value)
+    return ! hasNumeric? {'hasNumeric': true}:null
+}
+
+forbiddenEmails(control:FormControl): Promise<any> |Observable<any>{
+const prom= new Promise<any>((resolve,reject)=>{
+  setTimeout(() => {
+    if(control.value ==='joe@gmail.com'){
+      resolve({'Forbidden': true})
+    }
+    else{
+      resolve(null)
+    }
+}, 1500);
+});
+
+return prom
+
+}
 
 }
